eb294c08536d86e12de1abf1a4cfe238df37b277
Arnav Kapoor <arnavk805@gmail.com>
2018-08-10 16:38:08 +0100

    script to get rt format

--- raw_data/network/cpuspecial/movescript.sh
+++ raw_data/network/cpuspecial/movescript.sh
@@ -1,8 +1,14 @@
 mkdir -p cpuminimumsorted
 for files in ./*
 do
+	refile=`basename $files`
+	refile2=`echo $refile|cut -d "-" -f1`
+	if [[ "$refile" != "$refile2" ]]
+	then
+		refile2="$refile2.csv" 
+	fi
 	if [ ${files: -4} == ".csv" ]
 	then
-		mv "$files" ./cpuminimumsorted/
+		mv "$files" ./cpuminimumsorted/"$refile2"
 	fi
 done
