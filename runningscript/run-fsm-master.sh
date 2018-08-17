96a615a1a0b8d0b203ab4c61d63fb676320d14e2
Vanya Yaneva <s0835905@sms.ed.ac.uk>
2018-08-17 17:01:14 +0100

    Some helpful outputs in master running script

--- runningscript/run-fsm-master.sh
+++ runningscript/run-fsm-master.sh
@@ -6,11 +6,17 @@ $base_folder=~/fsmtesting/cleanedfsm/raw_data/201808/denseresults
 mkdir -p $base_folder
 
 # dense unsorted
+echo "Unsorted padded"
 ./run-fsm.sh "$base_folder/unsorted/padded" 0 0 N
+echo "Unsorted padded-transposed"
 ./run-fsm.sh "$base_folder/unsorted/padded-transposed" 0 1 N
+echo "Unsorted with-offsets"
 ./run-fsm.sh "$base_folder/unsorted/with-offsets" 1 0 N
 
 # dense sorted
+echo "Sorted padded"
 ./run-fsm.sh "$base_folder/sorted/padded" 0 0 Y
+echo "Sorted padded-transposed"
 ./run-fsm.sh "$base_folder/sorted/padded-transposed" 0 1 Y
+echo "Sorted with-offsets"
 ./run-fsm.sh "$base_folder/sorted/with-offsets" 1 0 Y
